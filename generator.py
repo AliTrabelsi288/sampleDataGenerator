149ea474ca9e3635462570f725b54c5f746568f7
Ali Trabelsi <ali.trabelsi@hotmail.co.uk>
2025-06-24 20:44:52 +0100

    Implemention for Ticket OT-14

--- generator.py
+++ generator.py
@@ -2,18 +2,27 @@ import sys
 import json
 from faker import Faker
 
+import xml.etree.ElementTree as ET
+import csv
+
 def generator(*args):
     fake = Faker()
     sample_data = []
     extracted_flags = set()
+    valid_prefixes = ['-help', '-fn', '-sn', '-d', '-ip', '-r', '-tf', '-g', '-ot', '-oj', '-oxml', '-ocsv', '-okv', '-otxt']
 
     # Default values
     min_num = 0
     max_num = 1000
     generate_count = 0
+
+    #Output type
     output_to_terminal = '-ot' in args
     output_to_json = '-oj' in args
-    valid_prefixes = ['-help', '-fn', '-sn', '-d', '-ip', '-r', '-tf', '-g', '-ot', '-oj']
+    output_to_xml = '-oxml' in args
+    output_to_csv = '-ocsv' in args
+    output_to_kvp = '-okv' in args
+    output_to_txt = '-otxt' in args
 
     if not args:
         print("*** No Flags Provided, Use 'python generator.py -help' for a List of All Commands ***")
@@ -31,6 +40,10 @@ def generator(*args):
         print("-tf   : True/False")
         print("-g    : Number of Records to Generate (e.g. -g10)")
         print("-ot   : Output to Terminal")
+        print("-oxml : Output to XML File")
+        print("-ocsv : Output to CSV File")
+        print("-okv  : Output as Key Value Pairs")
+        print("-otxt : Output to TXT File")
         print("-oj   : Output to JSON File\n")
         print("Example Minimal Usage:\n  python generator.py -fn -g10 -oj")
         print("This Will Generate 10 Records with First Names and Save Them to a JSON File.")
@@ -90,11 +103,47 @@ def generator(*args):
     # Output
     if output_to_terminal:
         print(json.dumps(sample_data))
+
     elif output_to_json:
         with open("data.json", "w") as f:
             json.dump(sample_data, f)
+
+    elif output_to_xml:
+        root = ET.Element("records")
+
+        for record in sample_data:
+            item = ET.SubElement(root, "record")
+            for key, value in record.items():
+                field = ET.SubElement(item, key)
+                field.text = str(value)
+
+        tree = ET.ElementTree(root)
+        tree.write("data.xml", encoding="utf-8", xml_declaration=True)
+
+    elif output_to_csv:
+        headers = sample_data[0].keys()     # Get CSV headers from keys of first Dictionary
+
+        with open("data.csv", mode="w", newline="", encoding="utf-8") as file:
+            writer = csv.DictWriter(file, fieldnames=headers)
+            writer.writeheader()
+            for row in sample_data:
+                writer.writerow(row)
+
+    elif output_to_kvp:
+        with open("data.kpv", "w", encoding="utf-8") as file:
+            for record in sample_data:
+                for key, value in record.items():
+                    file.write(f"{key}={value}\n")
+                file.write("\n")
+
+    elif output_to_txt:
+        with open("data.txt", "w", encoding="utf-8") as f:
+            for record in sample_data:
+                line = ", ".join(str(value) for value in record.values())
+                f.write(line + "\n")
+
     else:
-        print("*** Please Use the '-ot' or '-oj' Flags to Output Data ***")
+        print("*** Please Use a Valid Output Flag to Output the Data ***")
         return
 
 
